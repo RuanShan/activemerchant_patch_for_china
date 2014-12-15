ead71ea3bb7f67eef6f95036dfdb4b58686773eb
qinghe <david@dalian-david.onsute.com>
2014-12-15 00:28:41 -0800

    add constant ActionViewHelper

--- lib/offsite_payments_patch.rb
+++ lib/offsite_payments_patch.rb
@@ -1,14 +1,10 @@
-puts "yes, start load....."
-
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     module Integrations #:nodoc:
       Helper = OffsitePayments::Helper
       Notification =  OffsitePayments::Notification
       Return =  OffsitePayments::Return
+      ActionViewHelper = OffsitePayments::ActionViewHelper
     end
   end
 end
-
-puts "helper=?"
-puts ActiveMerchant::Billing::Integrations::Helper
